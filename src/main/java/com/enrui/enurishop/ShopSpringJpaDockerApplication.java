8f632c95daec3c44e29bacdee38e967bdf578cda
DATA1_ChoWangi <kiwancho@enuri.com>
2023-04-17 20:47:07 +0900

    springboot + JPA 환경설정 서버 구동시 mysql 접속이 안된다. -> 문제해결 필요

---
+++ src/main/java/com/enrui/enurishop/ShopSpringJpaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.enrui.enurishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpringJpaDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSpringJpaDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enrui/enurishop/ShopSpringJpaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrui.enurishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSpringJpaDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
